22e2531b1d05b62b486f603874f6d75a3c7fd482
gergely.m <m.gergely1999@gmail.com>
2024-01-27 14:21:55 +0100

    display received rgb matrix

--- backend/client_example.py
+++ backend/client_example.py
@@ -1,5 +1,10 @@
 import socket
 import time
+import numpy as np
+import io
+import gzip
+from pickle import dumps, loads
+
 
 ADDRESS = "127.0.0.1"
 UDP_PORT = 8001
@@ -8,7 +13,34 @@ addr = socket.getaddrinfo(
 	ADDRESS, UDP_PORT,
 	socket.AF_INET, socket.SOCK_DGRAM)[0]
 
+def make_data_to_send():
+	width = 64
+	height = 64
+
+	base_color = [255, 255, 255]
+	obstacle_color = [192, 192, 192]
+	unit_color = [0, 102, 204]
+
+	board = np.zeros((height, width, 3))
+	board[board[:, :, 0] == 0] = base_color
+
+	obstacle_chance = 0.5
+	obstacle_coords = np.where(np.random.rand(height, width) < obstacle_chance)
+	board[obstacle_coords] = obstacle_color
+
+	unit_chance = 0.1
+	unit_coords = np.where(np.random.rand(height, width) < unit_chance)
+	board[unit_coords] = unit_color
+
+	return board
+
+
 with socket.socket(*addr[:3]) as s:
 	s.connect(addr[4])
-	
-	s.send(b'hello')
+
+	for i in range(100):
+			
+		board = make_data_to_send()
+		compressed = gzip.compress(board.tobytes())
+		
+		s.send(compressed)
--- backend/main.py
+++ backend/main.py
@@ -1,11 +1,15 @@
 from fastapi import FastAPI, WebSocket, WebSocketDisconnect
 from fastapi.responses import HTMLResponse
 import asyncio
+import numpy as np
+import io
+import gzip
+import json
 
 from typing import Dict, Tuple
 
 
-ADDRESS = "127.0.0.1"
+ADDRESS = "0.0.0.0"
 UDP_PORT = 8001
 
 
@@ -19,25 +23,120 @@ html = """
 <html>
 	<head>
 		<title>TrainStreamer</title>
+		<style>
+			#container {
+			   border-collapse: collapse;
+			}
+
+			/* And this to your table's `td` elements. */
+			#container td {
+			   padding: 0;
+			   margin: 0;
+			   width: 10px;
+			   height: 10px;
+			}
+
+			#slidecontainer {
+				display: flex;
+				flex-direction: row;
+			}
+		</style>
 	</head>
 	<body>
-		<h1>WebSocket Test</h1>
+		<h1>Lux AI 2</h1>
 		<div>Hello there!</div>
+		<div id="slidecontainer">
+			<button id="decreaseState" type="button">-</button>
+			<input type="range" min="1" max="1" value="1" class="slider" id="stateSlider">
+			<button id="increaseState" type="button">+</button>
+		</div>
+		<span>State:</span>
+		<span id="currentState" style="font-weight:bold;color:red">1</span>
 		<div id="messages"></div>
+		<table id="container">
+
+		</table>
 		<script>
-			var client_id = Date.now()
+			var container = document.querySelector("table#container");
+			var slider = document.querySelector("input#stateSlider");
+			var decreaseStateButton = document.querySelector("button#decreaseState");
+			var increaseStateButton = document.querySelector("button#increaseState");
+			var currentStateElement = document.querySelector("span#currentState");
+			var states = [];
+			var current_state = 1;
+
+			slider.oninput = function() {
+				current_state = this.value;
+
+				console.log("Slider set to", current_state);
+				currentStateElement.innerHTML = current_state + " / " + states.length;
+				draw_table(current_state - 1);
+			}
+
+			decreaseStateButton.onclick = function() {
+				if(current_state <= 1) {
+					return;
+				}
+				current_state -= 1;
+				slider.setAttribute("value", current_state);
+				console.log("Slider set to", current_state);
+				currentStateElement.innerHTML = current_state + " / " + states.length;
+				draw_table(current_state - 1);
+			}
+
+			increaseStateButton.onclick = function() {
+				if(current_state >= states.length) {
+					return;
+				}
+				current_state += 1;
+				slider.setAttribute("value", current_state);
+				console.log("Slider set to", current_state);
+				currentStateElement.innerHTML = current_state + " / " + states.length;
+				draw_table(current_state - 1);
+			}
+
+			function adjust_slider(){
+				slider.setAttribute("max", states.length);
+				currentStateElement.innerHTML = current_state + " / " + states.length;
+			}
+
+			function draw_table(state){
+				if(state >= states.length) {
+					console.log("Can't draw index", state);
+					return;
+				}
+
+				console.log("Drawing index", state);
+
+				var arrays = states[state];
+				container.innerHTML = '';
+				for (var i = 0; i < arrays.length; i++) {
+					var row = container.insertRow(i);
+
+					for (var j = 0; j < arrays[i].length; j++) {
+						var cell = row.insertCell(j);
+
+						var red = arrays[i][j][0];
+						var green = arrays[i][j][1];
+						var blue = arrays[i][j][2];
+
+						cell.style.backgroundColor = "rgb(" + red + ", " + green + ", " + blue + ")";
+						cell.style.border = "1px solid lightgray";
+					}
+				}
+			}
+
+			var client_id = Date.now();
 			var ws = new WebSocket(`ws://localhost:8000/ws/${client_id}`);
 			ws.onmessage = function(event) {
-				var promise = event.data.text();
+				var arrays_str = event.data;
+				var arrays = JSON.parse(arrays_str);
+				states.push(arrays);
+				adjust_slider();
 
-				var messages = document.getElementById('messages')
-				var message = document.createElement('li')
-				var res = promise.then(r => {
-					console.log("Recieved:", r);
-					var content = document.createTextNode(r);
-					message.appendChild(content)
-				});
-				messages.appendChild(message)
+				if(states.length == 1) {
+					draw_table(0);
+				}
 			};
 			function sendMessage(event) {
 				ws.send("hi")
@@ -103,11 +202,16 @@ class UDPEndpoint(asyncio.DatagramProtocol):
 		print(f"Clients connected: {manager.active_connections}")
 
 	def datagram_received(self, data: bytes, addr: Tuple[str, int]) -> None:
-		print(f"Received {data} from {addr}")
-		print(f"Sendint to {len(manager.active_connections)} clients.")
+		print(f"Received data from {addr}")
+		print(f"Sending to {len(manager.active_connections)} clients.")
 
-		data_str = data.decode("utf-8")
-		self.loop.create_task(manager.broadcast(data))
+		decompressed_data = gzip.decompress(data)
+		arr = np.frombuffer(decompressed_data, dtype=np.float64).reshape(64, 64, 3)
+		arr_list = arr.tolist()
+		arr_json = json.dumps(arr_list)
+
+		print(f"Received {len(data)} bytes via UDP")
+		self.loop.create_task(manager.broadcast(arr_json))
 
 
 @app.on_event("startup")
