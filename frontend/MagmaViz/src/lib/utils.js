fb8377cfd1b9f3acbf7cfc9b6ce5fd2cafe3e914
gergely.m <m.gergely1999@gmail.com>
2024-02-05 23:56:38 +0100

    💄 change plain JS to Svelte templating where possible

---
+++ frontend/MagmaViz/src/lib/utils.js
@@ -0,0 +1,16 @@
+export async function GET(url) {
+    try {
+        const response = await fetch(url);
+        return await response.json();
+    } catch (error) {
+        console.error('Error fetching data:', error);
+        throw error;
+    }
+}
+
+export function checkIfNaN(value, default_value = -1) {
+    if (isNaN(value)) {
+        return default_value;
+    }
+    return value
+}
